4e60a349ede3b1ece3310bd9b9659e8113731e09
defektive <defektive@users.noreply.github.com>
2024-09-23 08:42:55 -0600

    self update

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ package main
 
 import (
 	"github.com/analog-substance/carbon/pkg/cmd"
+	"github.com/analog-substance/util/cli/updater/cobra_helper"
 	ver "github.com/analog-substance/util/cli/version"
 )
 
@@ -31,5 +32,6 @@ var commit = "replace"
 
 func main() {
 	cmd.RootCmd.Version = ver.GetVersionInfo(version, commit)
+	cmd.RootCmd.AddCommand(cobra_helper.CobraUpdateCmd)
 	cmd.Execute()
 }
