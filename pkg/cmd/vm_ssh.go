9a48a88f04c7b5029b7c3d57a043a97447e0a252
defektive <defektive@users.noreply.github.com>
2024-10-09 22:05:59 -0600

    autostart

--- pkg/cmd/vm_ssh.go
+++ pkg/cmd/vm_ssh.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/analog-substance/carbon/pkg/types"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 // vmSSH represents the config command
@@ -25,6 +27,16 @@ over the carbon process. So SSH agents should just work.
 		if len(vms) > 1 {
 			fmt.Println("Too many vms specified.")
 		} else if len(vms) == 1 {
+			if vms[0].State() != types.StateRunning.Name {
+				if AskIfSure("VM is stopped. Would you like to start it?") {
+					err := vms[0].Start()
+					if err != nil {
+						fmt.Printf("Error starting VM: %s\n", err)
+					}
+					time.Sleep(2 * time.Second)
+				}
+			}
+
 			err := vms[0].ExecSSH(user, args...)
 			if err != nil {
 				log.Error("failed to ssh to vm", "name", vms[0].Name(), "err", err)
